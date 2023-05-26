5785ca32ba623dd40fad45e622da6930ab76693f
Abulqasim <abulqasim.nazarov@gmail.com>
2023-05-26 14:09:39 +0400

    Add Button

--- Keyboard/KeyboardApp/KeyboardApp/Form1.Designer.cs
+++ Keyboard/KeyboardApp/KeyboardApp/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.buttonA = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.buttonShift = new System.Windows.Forms.Button();
+            this.buttonZ = new System.Windows.Forms.Button();
+            this.buttonQ = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonA
@@ -36,18 +40,71 @@
             this.buttonA.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.buttonA.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonA.ForeColor = System.Drawing.SystemColors.Info;
-            this.buttonA.Location = new System.Drawing.Point(12, 247);
+            this.buttonA.Location = new System.Drawing.Point(32, 310);
             this.buttonA.Name = "buttonA";
-            this.buttonA.Size = new System.Drawing.Size(29, 27);
+            this.buttonA.Size = new System.Drawing.Size(48, 47);
             this.buttonA.TabIndex = 0;
-            this.buttonA.Text = "A";
+            this.buttonA.Text = "a";
             this.buttonA.UseVisualStyleBackColor = false;
+            this.buttonA.Click += new System.EventHandler(this.buttonA_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 36);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(776, 167);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "";
+            // 
+            // buttonShift
+            // 
+            this.buttonShift.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonShift.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonShift.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonShift.ForeColor = System.Drawing.SystemColors.Info;
+            this.buttonShift.Location = new System.Drawing.Point(4, 353);
+            this.buttonShift.Name = "buttonShift";
+            this.buttonShift.Size = new System.Drawing.Size(76, 47);
+            this.buttonShift.TabIndex = 2;
+            this.buttonShift.Text = "⬆";
+            this.buttonShift.UseVisualStyleBackColor = false;
+            this.buttonShift.Click += new System.EventHandler(this.buttonShift_Click);
+            // 
+            // buttonZ
+            // 
+            this.buttonZ.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonZ.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonZ.ForeColor = System.Drawing.SystemColors.Info;
+            this.buttonZ.Location = new System.Drawing.Point(77, 353);
+            this.buttonZ.Name = "buttonZ";
+            this.buttonZ.Size = new System.Drawing.Size(48, 47);
+            this.buttonZ.TabIndex = 3;
+            this.buttonZ.Text = "z";
+            this.buttonZ.UseVisualStyleBackColor = false;
+            this.buttonZ.Click += new System.EventHandler(this.buttonZ_Click);
+            // 
+            // buttonQ
+            // 
+            this.buttonQ.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonQ.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonQ.ForeColor = System.Drawing.SystemColors.Info;
+            this.buttonQ.Location = new System.Drawing.Point(4, 267);
+            this.buttonQ.Name = "buttonQ";
+            this.buttonQ.Size = new System.Drawing.Size(48, 47);
+            this.buttonQ.TabIndex = 4;
+            this.buttonQ.Text = "q";
+            this.buttonQ.UseVisualStyleBackColor = false;
+            this.buttonQ.Click += new System.EventHandler(this.buttonQ_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonQ);
+            this.Controls.Add(this.buttonZ);
+            this.Controls.Add(this.buttonShift);
+            this.Controls.Add(this.textBox);
             this.Controls.Add(this.buttonA);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,5 +115,9 @@
         #endregion
 
         private Button buttonA;
+        private RichTextBox textBox;
+        private Button buttonShift;
+        private Button buttonZ;
+        private Button buttonQ;
     }
 }
--- Keyboard/KeyboardApp/KeyboardApp/Form1.cs
+++ Keyboard/KeyboardApp/KeyboardApp/Form1.cs
@@ -6,5 +6,50 @@ namespace KeyboardApp
         {
             InitializeComponent();
         }
+
+        private void buttonA_Click(object sender, EventArgs e)
+        {
+            if (this.buttonA.Text == "a")
+                this.textBox.Text += "a";
+            else
+                this.textBox.Text += "A";    
+        }
+
+        private void buttonShift_Click(object sender, EventArgs e)
+        {
+            if (this.buttonA.Text == "a")
+                this.buttonA.Text = "A";
+            else
+                this.buttonA.Text = "a";
+
+            if (this.buttonZ.Text == "z")
+                this.buttonZ.Text = "Z";
+            else
+                this.buttonZ.Text = "z";
+
+
+
+            if (this.buttonQ.Text == "q")
+                this.buttonQ.Text = "Q";
+            else
+                this.buttonQ.Text = "q";
+
+        }
+
+        private void buttonZ_Click(object sender, EventArgs e)
+        {
+            if (this.buttonZ.Text == "z")
+                this.textBox.Text += "z";
+            else
+                this.textBox.Text += "Z";
+        }
+
+        private void buttonQ_Click(object sender, EventArgs e)
+        {
+            if (this.buttonQ.Text == "q")
+                this.textBox.Text += "q";
+            else
+                this.textBox.Text += "Q";
+        }
     }
 }
